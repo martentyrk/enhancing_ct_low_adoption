a04d4ebad80f422639d727a5d3355b10fe06d935
Rob Romijnders FS2.DAS5 <romijndersrob@gmail.com>
2023-06-21 10:44:14 +0200

    put covasim in scripts/preamble.

--- scripts/preamble.sh
+++ scripts/preamble.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/var/scratch/${USER}/projects/gsl/lib
-export PYTHONPATH=$PYTHONPATH:/var/scratch/${USER}/projects/CRISP/code:/var/scratch/${USER}/projects/dpfn:/var/scratch/${USER}/projects/abm/src:/var/scratch/${USER}/projects/sib
+export PYTHONPATH=$PYTHONPATH:/var/scratch/${USER}/projects/CRISP/code:/var/scratch/${USER}/projects/dpfn:/var/scratch/${USER}/projects/abm/src:/var/scratch/${USER}/projects/sib:/var/scratch/${USER}/projects/covasim
 export PATH=/var/scratch/${USER}/conda/envs/ml38/bin/:$PATH
