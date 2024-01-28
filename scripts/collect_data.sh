0c03693dbc621486b1a980e41d3670dd5f3cad73
Rob Carbon <romijndersrob@gmail.com>
2024-01-28 10:02:47 +0100

    Add comments in sbatch script for collect_data.

--- scripts/collect_data.sh
+++ scripts/collect_data.sh
@@ -10,12 +10,17 @@
 
 
 #To run, do sbatch scripts/collect_data_cluster.sh
-dirname='var/scratch/rromijnd/projects/dpfn/results/trace_run_prequential/intermediate_graph_cv_01__model_CV01/test_with_obs_30'
+dirname='/var/scratch/rromijnd/projects/dpfn/results/trace_run_prequential/intermediate_graph_cv_01__model_CV01/test_with_obs_30'
 dirname_out=${dirname}_out
+echo "dirname_out will be ${dirname_out}"
+
+echo "Start concatenation"
 
 # Concatenate and shuffle train and validation sets
 cat ${dirname}/positive_*.jl > ${dirname}/positive.jlconcat
 cat ${dirname}/negative_*.jl > ${dirname}/negative.jlconcat
+
+echo "Start shuffling"
 
 shuf ${dirname}/positive.jlconcat > ${dirname}/positive.jlconcat.shuf
 shuf ${dirname}/negative.jlconcat > ${dirname}/negative.jlconcat.shuf
@@ -27,6 +32,7 @@ echo "Positive samples": `wc -l ${dirname}/positive.jlconcat.shuf`
 echo "Negative samples": `wc -l ${dirname}/negative.jlconcat.shuf`
 
 # Subsample negative samples
+echo "Start subsampling"
 num_positive=`wc -l ${dirname}/positive.jlconcat.shuf | awk '{print $1}'`
 head -n ${num_positive} ${dirname}/negative.jlconcat.shuf > ${dirname}/negative.jlconcat.shuf.subsampled
 echo "Subsampled to match positive samples:" `wc -l ${dirname}/negative.jlconcat.shuf.subsampled`
@@ -45,6 +51,7 @@ rm ${dirname}/all.jlconcat.shuf
 [ -d ${dirname_out} ] || mkdir ${dirname_out}
 
 # Copy to dir output
+echo "Start copy of final dataset"
 cp ${dirname}/all.jlconcat.shuf.shuf ${dirname_out}/all.jl
 rm ${dirname}/all.jlconcat.shuf.shuf
 
